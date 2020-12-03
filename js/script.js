1c43c3a1dcee9c0da8bdac0f82cc68501ef91053
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-03 16:07:12 +0100

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,131 @@
+// definire un array di oggetti; ogni oggetto
+// rappresenta un'icona, che è caratterizzata da:
+// nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template
+// literal, visualizzare in pagina tutte le icone con il
+// proprio nome.
+
+$(document).ready(
+  function(){
+
+    const icons = [
+      {
+        name: "dove",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "otter",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "sleigh",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "kiwi-bird",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "spider",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "apple-alt",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "carrot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "lemon",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "dragon",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "pepper-hot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "space-shuttle",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "truck-monster",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "wheelchair",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "tram",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "fighter-jet",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "helicopter",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+    ];
+
+    const container=$(".box");
+    print(icons,container);
+
+
+  }
+);
+
+
+
+
+function print(array,container){
+  array.forEach((element,index) => {
+
+    const {name}=element;
+    container.append(`
+        <div>${name.toUpperCase()}</div>
+        <i class="fas fa${name}"></i>
+      `);
+
+  });
+
+}
